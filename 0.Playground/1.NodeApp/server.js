b5ecb01f023e0e73a5c409b19fd9c0cc881034ab
nest <nsitseo@gmail.com>
2022-10-12 12:42:47 +0530

    update

--- 0.Playground/1.NodeApp/server.js
+++ 0.Playground/1.NodeApp/server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config({ path: 'config.env' });
 const app = express();
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const fs = require('fs');
+const pug = require('pug');
+const expressLogger = require('express-logger');
+
+require('dotenv').config({ path: 'config.env' });
 const PORT = process.env.PORT || 5000;
 const posts = require('./routes/postRouter');
 const users = require('./routes/userRouter');
 
+// Database
 const DB = process.env.DATABASE.replace(
     '<password>',
     process.env.DATABASE_PASSWORD
 );
-
 mongoose
     .connect(DB, {
         useNewUrlParser: true,
@@ -19,12 +28,29 @@ mongoose
         console.log('DB connection successful');
     });
 
+// Middlewares
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+app.use(expressLogger({ path: 'logfile.txt' }));
 
+// Pug template serve
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-    res.send(
-        '<h1>NodeApp</h1><a target="_blank" href="http://localhost:5000/users">Users</a></br><a target="_blank" href="http://localhost:5000/posts">Posts</a></br></br><p>Users supports Get,Post,and search by Name query</p><p>Posts supports Get,Post,and search by Title query</p>'
-    );
+    res.render('home', { title: 'Home' });
+    // res.send(
+    //     '<h1>NodeApp</h1><a target="_blank" href="http://localhost:5000/users">Users</a></br><a target="_blank" href="http://localhost:5000/posts">Posts</a></br></br><p>Users supports Get and Post queries</p><p>Posts supports Get and Post queries</p>'
+    // );
+});
+
+// Filesystem write read
+fs.writeFileSync(
+    './test.txt',
+    JSON.stringify({ name: 'test', email: 'test@test.com' })
+);
+fs.readFile('./test.txt', (err, data) => {
+    console.log(JSON.parse(data));
 });
 
 app.use('/posts', posts);
